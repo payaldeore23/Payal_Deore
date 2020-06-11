4063a0021a4a922b1f40cf5581c72864bf02c82b
payaldeore23 <66246254+payaldeore23@users.noreply.github.com>
2020-06-11 12:08:45 +0530

    Create student.c

---
+++ student.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct student{
+    int rollno;
+    char name[20];
+    int age;
+    struct subjects{
+        float English;
+        float Maths;
+        float Science;
+    }t1;
+};
+
+int main()
+{
+    double total=0;
+    struct student s1={12,"Christ",22};
+    printf("Roll no : %d\n Name : %s\n Age : %d\n",s1.rollno,s1.name,s1.age);
+
+    struct subjects t1={92,78,82};
+    printf("English = %.2f\n Maths = %.2f\n Science = %.2f\n",t1.English,t1.Maths,t1.Science);
+
+    total += t1.English + t1.Maths + t1.Science;
+    printf("Total Score = %.2lf",total);
+
+    return 0;
+}
