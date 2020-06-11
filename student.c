7ef9c9f74d0f00a4b65c0b4767aa0379008e8c20
payaldeore23 <66246254+payaldeore23@users.noreply.github.com>
2020-06-11 12:25:35 +0530

    Update student.c

--- student.c
+++ student.c
@@ -14,7 +14,12 @@ struct student{
 int main()
 {
     double total=0;
-    struct student s1={12,"Christ",22};
+    struct student s1{
+    .roll no = 12,
+    .name = "Christ",
+    .age = 25
+    };
+    printf("size = %d\n",sizeof(struct student));
     printf("Roll no : %d\n Name : %s\n Age : %d\n",s1.rollno,s1.name,s1.age);
 
     struct subjects t1={92,78,82};
